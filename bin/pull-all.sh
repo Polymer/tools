597dcfd60e9939b1a4a96b68603676b746a29532
Daniel Freedman <dfreedm@google.com>
2013-08-23 15:19:06 -0700

    Move all looping into shared repo_conf
    - Due to lack of hash maps on Mac OS X bash (too old), looping has to use two arrays and indexing

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -2,18 +2,7 @@
 
 # This script will check out all of the Polymer project's repositories
 # Load repo configuration
-. ../conf/repo_conf.sh
+. "`dirname $0`/../conf/repo_conf.sh"
 
-for REPO in ${POLYMER_REPOS[@]}; do
-  if [ -d $REPO ]; then
-    pull $REPO
-  else
-    clone $REPO "master"
-  fi
-done
-
-if [[ ${#FAILED[@]} -gt 0 ]]; then
-  err
-else
-  ok
-fi
+# Synchronize repos
+sync_repos
--- conf/repo_conf.sh
+++ conf/repo_conf.sh
@@ -28,21 +28,22 @@ REPOS=(
   labs
 )
 
-# Web Animations has its own org, hardcode full path
-WEB_ANIMATIONS_REPO="https://github.com/web-animations/web-animations-js.git"
+# Array of all the repos with full path
+REPO_PATHS=()
 
-# Will house all the repos with full path
-POLYMER_REPOS=();
-
-for REPO in ${POLYMER_REPOS[@]}; do
-  POLYMER_REPOS+=("$POLYMER_PATH/$REPO.git")
+for REPO in ${REPOS[@]}; do
+  REPO_PATHS+=("$POLYMER_PATH/$REPO.git")
 done
-POLYMER_REPOS+=("$WEB_ANIMATIONS_REPO");
+
+# Web Animations has its own org, hardcode full path
+REPOS+=("web-animations-js")
+REPO_PATHS+=("https://github.com/web-animations/web-animations-js.git")
 
 # repos that fail to clone will be put here
 FAILED=()
 
-REPO_BRANCH="master"
+# default branch of clones
+DEFAULT_BRANCH="master"
 
 err() {
   echo -e "\033[1;31m${#FAILED[@]} REPOS FAILED TO CHECK OUT!\033[0m"
@@ -53,6 +54,7 @@ err() {
   read
 }
 
+# ARGS: $1 log message, $2 repo shortname
 log() {
   echo -e "\033[1;37m===== $1 \033[1;34m$2 \033[1;37m=====\033[0m"
 }
@@ -61,19 +63,50 @@ ok() {
   echo -e "\033[1;32mOK\033[0m"
 }
 
-pull() {
-    pushd $1 >/dev/null 2>&1
-    log "PULLING" "$1"
-    git pull "$2" --rebase
-    if [ $? -ne 0 ]; then
-      FAILED+=($REPO)
-    else
-      git submodule update --init --recursive
-    fi
-    popd >/dev/null 2>&1
+# Prints errors or says OK
+status_report() {
+  if [[ ${#FAILED[@]} -gt 0 ]]; then
+    err
+  else
+    ok
+  fi
 }
 
+# ARGS: $1 shortname, $2 branch
+pull() {
+  pushd $1 >/dev/null 2>&1
+  log "PULLING" "$1"
+  # argument arity is important for some reason :(
+  if [[ -z "$2" ]]; then
+    git pull --rebase
+  else
+    git pull --rebase origin "$2"
+  fi
+  if [ $? -ne 0 ]; then
+    FAILED+=($1)
+  else
+    git submodule update --init --recursive
+  fi
+  popd >/dev/null 2>&1
+}
+
+# ARGS: $1 shortname, $2 branch
 clone() {
   log "CLONING" "$1"
+  branch="$2" || "$DEFAULT_BRANCH"
   git clone -b "$2" --recursive "$1"
+}
+
+# ARGS: $1 branch
+sync_repos() {
+  for i in ${!REPOS[@]}; do
+    REPO="${REPOS[$i]}"
+    if [ -d $REPO ]; then
+      pull "$REPO" "$1"
+    else
+      clone "${REPO_PATHS[$i]}" "$1"
+    fi
+  done
+
+  status_report
 }
