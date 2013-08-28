dc49a1bdc6fc5924dd5a99330dee58a6259f7bf5
Daniel Freedman <dfreedm@google.com>
2013-08-28 15:45:57 -0700

    Flesh out make-release
    - clone/checkout repos
    - tag repos
    - generate changelog from last tag
    - test and build polymer
    - zip everything up

--- bin/make-release.sh
+++ bin/make-release.sh
@@ -3,77 +3,90 @@
 . "`dirname $0`/pull-all.sh"
 
 # version number
+## TODO(dfreedman): date stamped for now, follow polymer/package.json in the future
+## make sure to update polymer/package.json to reflect this value
 VERSION=`date "+v0.0.%Y%m%d"`
 
 # ssh auth, easier to script
 POLYMER_PATH="git@github.com:Polymer"
 
+# Changelog format: - commit message ([commit](commit url on github))
+PRETTY="- %s ([commit](https://github.com/Polymer/$REPO/commit/%h))"
+
 tag_repos() {
   FAILED=()
-  for REPO in ${REPOS[@]}; do
-    pushd $REPO
-    log "TAGGING" "$REPO"
-    git tag "$VERSION"
-    if [ $? -ne 0 ]; then
-      FAILED+=($REPO)
-    fi
-    # push only tags
-    git push --tags
-    popd
-  done
-  status_report "TAG"
-}
-
-gen_changelog() {
-  touch "changelog.md"
   for REPO in ${REPOS[@]}; do
     # skip web animations repo
     if [ $REPO = 'web-animations-js' ]; then
       continue
     fi
-    # format: - commit message ([commit](commit url on github))
-    PRETTY="- %s ([commit](https://github.com/Polymer/$REPO/commit/%h))"
-    pushd $REPO
+    pushd $REPO >/dev/null
+    log "TAGGING" "$REPO"
+    git tag -f "$VERSION"
+    if [ $? -ne 0 ]; then
+      FAILED+=($REPO)
+    fi
+    # push only tags
+    git push --tags
+    popd >/dev/null
+  done
+  status_report "TAG"
+}
+
+gen_changelog() {
+  for REPO in ${REPOS[@]}; do
+    # skip web animations repo
+    if [ $REPO = 'web-animations-js' ]; then
+      continue
+    fi
+    pushd $REPO >/dev/null
     log "GEN CHANGELOG" "$REPO"
-    echo "#### $REPO\n" >> "changelog.md"
-    echo "`git log $OLD_VERSION..$VERSION --pretty=$PRETTY`" >> "changelog.md"
-    popd
+    # find slightly older tag, sorted alphabetically
+    OLD_VERSION="`git tag -l | tail -n 2 | head -n 1`"
+    if [[ -n $OLD_VERSION ]]; then
+      echo "#### $REPO" >> "../changelog.md"
+      git log $OLD_VERSION..$VERSION --pretty="$PRETTY" >> "../changelog.md"
+    fi
+    popd >/dev/null
   done
   ok
 }
 
 build() {
-  pushd polymer
+  pushd polymer >/dev/null
+  log "INSTALLING" "node modules"
+  npm --silent install
+  log "TESTING" "polymer"
   grunt test
-  if [ $? -eq 0 ]; then
-    # no version number on build file
-    grunt
-    # version number on build file
-    grunt "version:$VERSION"
-    ok
-  else
-    FAILED=(polymer)
-    err "FAILED TO BUILD"
+  if [ $? -ne 0 ]; then
+    err "polymer FAILED TESTING!"
+    die
   fi
-  popd
+  log "BUILDING" "polymer"
+  grunt
+  # version number on build file
+  cp polymer.min.js polymer-${VERSION}.min.js
+  cp polymer.min.js.map polymer-${VERSION}.min.js.map
+  mv build.log polymer{,-$VERSION}.min.js{,.map} ../
+  ok
+  popd >/dev/null
 }
 
 package() {
-  # polymer build only
-  zip -j -r polymer-build-$VERSION.zip polymer-$VERSION/{polymer/polymer,platform/platform}-$VERSION.min.js{,.map}
-  polymer-$VERSION/polymer/{build.log,AUTHORS,PATENTS,LICENSE,CONTRIBUTING.md,README.md}
-  # all repos build
-  zip -x "*.git*" -r polymer-all-$VERSION.zip polymer-$VERSION
+  log "ZIPPING" "ALL REPOS"
+  zip -q -x "polymer-$VERSION/polymer.min.js*" -x "*.git*" -x "*node_modules/*" -x "*tools/*" -r polymer-all-$VERSION.zip polymer-$VERSION
   ok
 }
 
 release() {
-  mkdir polymer-$VERSION
-  pushd polymer-$VERSION
+  mkdir -p polymer-$VERSION
+  pushd polymer-$VERSION >/dev/null
   sync_repos
   tag_repos
   gen_changelog
-  popd
   build
+  popd >/dev/null
   package
 }
+
+release
