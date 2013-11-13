9d9f5a0b8d70499d33ee0684944a884293541c98
Daniel Freedman <dfreedm@google.com>
2013-11-12 17:39:29 -0800

    Make expected projects and components structure
    Lookout if you run this blindly!

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 dir="${0%[/\\]*}"
-bash $dir/pull-all-polymer.sh $@
-bash $dir/pull-all-elements.sh $@
-bash $dir/pull-all-projects.sh $@
+! [ -d components ] && mkdir components
+pushd components
+bash ../$dir/pull-all-polymer.sh $@
+bash ../$dir/pull-all-elements.sh $@
+popd
+! [ -d projects ] && mkdir projects
+pushd projects
+bash ../$dir/pull-all-projects.sh $@
+popd
