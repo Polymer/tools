5fad8286721df774d3996b8aa02faa54b0b529b8
Daniel Freedman <dfreedm2@gmail.com>
2014-03-26 10:51:05 -0700

    Just use SSH by default

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -3,10 +3,10 @@
 dir="${0%[/\\]*}"
 ! [ -d components ] && mkdir components
 pushd components
-bash ../$dir/pull-all-polymer.sh $@
-bash ../$dir/pull-all-elements.sh $@
+bash ../$dir/pull-all-polymer.sh -s $@
+bash ../$dir/pull-all-elements.sh -s $@
 popd
 ! [ -d projects ] && mkdir projects
 pushd projects
-bash ../$dir/pull-all-projects.sh $@
+bash ../$dir/pull-all-projects.sh -s $@
 popd
