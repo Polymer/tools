f3ad180278a004ebcd6b16adf435bd5ef558ce5e
Daniel Freedman <dfreedm@google.com>
2014-05-07 15:02:58 -0700

    fix case where ~/node_modules became install target for bigstraw

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -3,6 +3,8 @@ dir="${0%[/\\]*}"
 pushd $dir
 git pull
 if [ ! -e node_modules/bigstraw ]; then
+  # ~/node_modules may be the installation target if local node_modules folder doesn't exist
+  mkdir node_modules
   npm install bigstraw
 fi
 popd
