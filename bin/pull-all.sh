9ae5d92deb5364c2745fc5e6f5d3422a2ffc3bfc
Daniel Freedman <dfreedm@google.com>
2014-05-07 12:46:35 -0700

    convert pull-all to bigstraw

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
 dir="${0%[/\\]*}"
-! [ -d components ] && mkdir components
-pushd components
-bash ../$dir/pull-all-polymer.sh -s $@
-bash ../$dir/pull-all-elements.sh -s $@
+pushd $dir
+npm install bigstraw
 popd
-! [ -d projects ] && mkdir projects
-pushd projects
-bash ../$dir/pull-all-projects.sh -s $@
-popd
+node $dir/node_modules/bigstraw/index.js $dir/../repo-configs/*.json $@
