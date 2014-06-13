26a7cfbffd72310d69bdf1c494f338f05435eb61
Daniel Freedman <dfreedm2@gmail.com>
2014-06-13 11:21:15 -0700

    always update bigstraw version

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -5,7 +5,7 @@ git pull
 if [ ! -e node_modules/bigstraw ]; then
   # ~/node_modules may be the installation target if local node_modules folder doesn't exist
   mkdir node_modules
-  npm install bigstraw
 fi
+npm install bigstraw
 popd
 node $dir/node_modules/bigstraw/index.js -s $dir/../repo-configs/{core,polymer}.json $@
