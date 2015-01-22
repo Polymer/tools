ee2ccdf15a886a1029e05f6868270e4b6ea35624
Ian MacLeod <imac@google.com>
2015-01-22 13:05:35 -0800

    Mostly working!

---
+++ scripts/postinstall.js
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+var selenium  = require('selenium-standalone');
+
+var BASE_VERSION   = '2.44.0';
+var CHROME_VERSION = '2.13';
+
+// Cross-platform drivers.
+var drivers = {
+  chrome: {
+    version: CHROME_VERSION,
+    arch:    process.arch,
+  },
+};
+
+if (process.platform === 'win32') {
+  drivers.ie = {
+    version: BASE_VERSION,
+    arch:    process.arch,
+  };
+}
+
+process.stdout.write('Downloading Selenium... ');
+selenium.install({version: BASE_VERSION, drivers: drivers}, function(error) {
+  if (error) {
+    process.stdout.write('error: ' + error + '\n');
+  } else {
+    process.stdout.write('done\n');
+  }
+});
