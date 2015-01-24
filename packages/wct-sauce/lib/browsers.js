79b2396f762134261795d346251a651af765a7f3
Ian MacLeod <imac@google.com>
2015-01-24 09:14:14 -0800

    Working sauce plugin

---
+++ packages/wct-sauce/lib/browsers.js
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+var _ = require('lodash');
+
+var DEFAULT_BROWSERS = require('../default-sauce-browsers.json');
+
+// "<PLATFORM>/<BROWSER>[@<VERSION>]"
+var BROWSER_SPEC = /^([^\/@]+)\/([^\/@]+)(?:@(.*))?$/;
+
+/**
+ * Expands an array of browser identifiers for sauce browsers into their
+ * webdriver capabilities objects.
+ *
+ * @param {!Object} pluginOptions
+ * @param {!Array.<string|!Object>} browsers
+ * @param {function(*, Array.<!Object>)} done
+ */
+function expand(pluginOptions, browsers, done) {
+  if (browsers.indexOf('default') !== -1 || browsers.indexOf('all')) {
+    // TODO(nevir): Figure out the latest version of each browser and pick
+    // appropriate spreads of versions & OSes.
+    browsers = browsers.concat(_.cloneDeep(DEFAULT_BROWSERS));
+    browsers = _.difference(browsers, ['default', 'all']);
+  }
+
+  browsers = _.compact(browsers.map(_expandBrowser));
+  browsers.forEach(_injectUrl.bind(null, pluginOptions));
+
+  done(null, browsers)
+}
+
+/**
+ * @param {string|!Object} browser
+ * @return {Object}
+ */
+function _expandBrowser(browser) {
+  if (_.isObject(browser)) return browser;
+  var match = _.isString(browser) && browser.match(BROWSER_SPEC);
+  if (!match) {
+    console.log('Invalid sauce browser spec:', browser);
+    return null;
+  }
+
+  return {
+    browserName: match[2],
+    platform:    match[1],
+    version:     match[3] || '',
+  };
+}
+
+/**
+ * @param {!Object} pluginOptions
+ * @param {!Object} browser
+ */
+function _injectUrl(pluginOptions, browser) {
+  browser.url = {
+    hostname:  'ondemand.saucelabs.com',
+    port:      80,
+    username:  pluginOptions.username,
+    accessKey: pluginOptions.accessKey,
+  };
+}
+
+module.exports = {
+  expand: expand,
+};
---
+++ packages/wct-sauce/lib/plugin.js
@@ -0,0 +1,84 @@
+/**
+ * @license
+ * Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+var _ = require('lodash');
+
+var browsers = require('./browsers');
+var sauce    = require('./sauce');
+
+/** WCT plugin that enables support for remote browsers via Sauce Labs. */
+module.exports = function(wct, pluginOptions) {
+
+  // The capabilities objects for browsers to run. We don't know the tunnel id
+  // until `prepare`, so we've gotta hang onto them.
+  var eachCapabilities = [];
+
+  wct.hook('configure', function(done) {
+    _.defaults(pluginOptions, {
+      username:  process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      tunnelId:  process.env.SAUCE_TUNNEL_ID,
+    });
+
+    var names = [];
+    if (pluginOptions.browsers && pluginOptions.browsers.length) {
+      // We support comma separated browser identifiers for convenience.
+      names = pluginOptions.browsers.join(',').split(',');
+    }
+    if (names.length === 0) return done();
+
+    browsers.expand(pluginOptions, names, function(error, expanded) {
+      if (error) return done(error);
+      wct.emit('log:debug', 'Using sauce browsers:', expanded);
+      // We are careful to append these to the configuration object, even though
+      // we don't know the tunnel id yet. This allows WCT to give a useful error
+      // if no browsers were configured.
+      var activeBrowsers = wct.options.activeBrowsers;
+      activeBrowsers.push.apply(activeBrowsers, expanded);
+      // But we still need to inject the sauce tunnel ID once we know it.
+      eachCapabilities = expanded;
+
+      done();
+    });
+  });
+
+  wct.hook('prepare', function(done) {
+    // Don't bother spinning up the tunnel if we don't have any browsers talking
+    // over it.
+    if (eachCapabilities.length === 0) return done();
+
+    // Is there already an active sauce tunnel?
+    if (pluginOptions.tunnelId) {
+      _injectTunnelId(eachCapabilities, pluginOptions.tunnelId);
+      return done();
+    }
+
+    // Let anyone know, and give them a chance to modify the options prior to
+    // booting up the Sauce Connect tunnel.
+    wct.emitHook('prepare:sauce-tunnel', function(error) {
+      if (error) return done(error);
+      sauce.startTunnel(pluginOptions, wct, function(error, tunnelId) {
+        if (error) return done(error);
+        _injectTunnelId(eachCapabilities, tunnelId);
+        done();
+      });
+    });
+  });
+
+};
+
+/**
+ * @param {!Array<!Object>} eachCapabilities
+ * @param {string} tunnelId
+ */
+function _injectTunnelId(eachCapabilities, tunnelId) {
+  eachCapabilities.forEach(function(browser) {
+    browser['tunnel-identifier'] = tunnelId;
+  });
+}
