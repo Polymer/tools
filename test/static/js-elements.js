3cec17041b07868a4bd990fdb2a7a840c127d00d
AJ Ortega <ajo@google.com>
2015-02-12 15:34:23 -0800

    Fix up element definitions. Sketch out test for Polymer() call.
    Test for basic element metadata.
    
    branch test/modulateFinder.js into test/elementFinder.js
    
    Detect existence of an element.
    
    Add a test for element names and ordering.
    
    Find element names.
    
    Finish parsing basic element metadata.

--- test/static/js-elements.js
+++ test/static/js-elements.js
@@ -1,7 +1,74 @@
-<script src="https://cdn.firebase.com/js/client/2.0.3/firebase.js"></script>
+/**
+ * I am a description of test-element.
+ */
+Polymer({
 
-<script>
+  is: 'test-element',
 
+  /**
+   * Fired when properties on `data` are added, removed, or modified.
+   *
+   * @event data-change
+   */
+
+  /**
+   * Fired when an error occurs on an interaction with Firebase.  The
+   * `details.error` property contains the `Error` object provided by
+   * the Firebase API.
+   *
+   * @event error
+   */
+
+  publish: {
+    /**
+     * I am a string!
+     */
+    stringProp: String,
+    /**
+     * I am a number!
+     */
+    numProp: Number,
+    /**
+     * I am an object!
+     */
+    objectProp: Object,
+    /**
+     * I am an object with explicit type!
+     * @type HTMLElement
+     */
+    elementProp: Object,
+    /**
+     * I am an object with notify=true!
+     */
+    objectNotify: {
+      type: Object,
+      notify: true
+    },
+    /**
+     * I am a boolean property!
+     */
+    boolProp: Boolean
+  },
+
+  bind: {
+    numProp: 'numChanged',
+    elementProp: 'elemChanged'
+  },
+
+  numChanged: function() {
+
+  },
+
+  elemChanged: function() {
+
+  }
+
+});
+
+
+/**
+ * I am a description of x-firebase.
+ */
 Polymer({
 
   is: 'x-firebase',
@@ -398,5 +465,3 @@ Polymer({
   }
 
 });
-
-</script>
