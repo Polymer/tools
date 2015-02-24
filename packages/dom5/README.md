21663e70c4a614339080d5ef4dfd2a999f710d2a
Justin Fagnani <justin@fagnani.com>
2015-02-24 15:24:37 -0800

    Add set/getAttribute

--- packages/dom5/README.md
+++ packages/dom5/README.md
@@ -1,1 +1,3 @@
 # parse5-utils
+
+Utilities for using parse5
---
+++ packages/dom5/parse5-utils.js
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+function getAttributeIndex(element, name) {
+  for (var i = 0; i < pathLib.attrs.length; i++) {
+    if (element.attrs[i].name == name) {
+      return i;
+    }
+  }
+  return null;
+}
+
+function getAttribute(element, name) {
+  var i = getAttributeIndex(element, name);
+  if (i) {
+    return element.attrs[i].value;      
+  }
+  return null;
+}
+
+function setAttribute(element, name, value) {
+  var i = getAttributeIndex(element, name);
+  if (i) {
+    element.attrs[i].value = value;
+  } else {
+    pathLib.attrs.push({name: name, value: value});
+  }
+}
+
+module.exports = {
+  getAttributeIndex: getAttributeIndex,
+  getAttribute: getAttribute,
+  setAttribute: setAttribute,
+};
