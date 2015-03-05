156bd8a1ea38b7628378da674ee356eee7d5ecfd
Daniel Freedman <dfreedm@google.com>
2015-03-05 11:23:47 -0800

    add tree walking utils
    Add `npm test` support, update chai
    
    fix spelling
    
    Add DOM hierarchy moving methods
    
    Add explicit AND, OR, and NOT predicates
    
    query and queryAll now take a single predicate function
    
    tests for querying
    
    Unbreak the tests by pushing element check into query/queryAll
    
    address nits
    
    remove node manipulation functions for a different PR

--- packages/dom5/parse5-utils.js
+++ packages/dom5/parse5-utils.js
@@ -8,12 +8,13 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
+// jshint node: true
+
 function getAttributeIndex(element, name) {
-  if (element.attrs) {
-    for (var i = 0; i < element.attrs.length; i++) {
-      if (element.attrs[i].name == name) {
-        return i;
-      }
+  var n = name.toLowerCase();
+  for (var i = 0; i < element.attrs.length; i++) {
+    if (element.attrs[i].name.toLowerCase() === n) {
+      return i;
     }
   }
   return -1;
@@ -36,8 +37,128 @@ function setAttribute(element, name, value) {
   }
 }
 
+function hasTagName(name) {
+  var n = name.toLowerCase();
+  return function(node) {
+    return node.tagName.toLowerCase() === n;
+  };
+}
+
+function hasClass(name) {
+  return function(node) {
+    var attr = getAttribute(node, 'class');
+    if (!attr) {
+      return false;
+    }
+    return attr.split(' ').indexOf(name) > -1;
+  };
+}
+
+/**
+ * OR an array of predicates
+ */
+function OR(/* ...rules */) {
+  var rules = new Array(arguments.length);
+  for (var i = 0; i < arguments.length; i++) {
+    rules[i] = arguments[i];
+  }
+  return function(node) {
+    for (var i = 0; i < rules.length; i++) {
+      if (rules[i](node)) {
+        return true;
+      }
+    }
+    return false;
+  };
+}
+
+/**
+ * AND an array of predicates
+ */
+function AND(/* ...rules */) {
+  var rules = new Array(arguments.length);
+  for (var i = 0; i < arguments.length; i++) {
+    rules[i] = arguments[i];
+  }
+  return function(node) {
+    for (var i = 0; i < rules.length; i++) {
+      if (!rules[i](node)) {
+        return false;
+      }
+    }
+    return true;
+  };
+}
+
+/**
+ * negate an individual predicate, or a group with AND or OR
+ */
+function NOT(predicateFn) {
+  return function(node) {
+    return !predicateFn(node);
+  };
+}
+
+function hasAttr(attr) {
+  return function(node) {
+    return getAttributeIndex(node, attr) > -1;
+  };
+}
+
+function hasAttrValue(attr, value) {
+  return function(node) {
+    return getAttribute(node, attr) === value;
+  };
+}
+
+function isElement(node) {
+  return node.nodeName === node.tagName;
+}
+
+function query(node, predicate) {
+  if (isElement(node) && predicate(node)) {
+    return node;
+  }
+  var match = null;
+  if (node.childNodes) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      match = query(node.childNodes[i], predicate);
+      if (match) {
+        break;
+      }
+    }
+  }
+  return match;
+}
+
+function queryAll(node, predicate, matches) {
+  if (!matches) {
+    matches = [];
+  }
+  if (isElement(node) && predicate(node)) {
+    matches.push(node);
+  }
+  if (node.childNodes) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      queryAll(node.childNodes[i], predicate, matches);
+    }
+  }
+  return matches;
+}
+
 module.exports = {
-  getAttributeIndex: getAttributeIndex,
   getAttribute: getAttribute,
   setAttribute: setAttribute,
+  isElement: isElement,
+  query: query,
+  queryAll: queryAll,
+  predicates: {
+    hasClass: hasClass,
+    hasAttr: hasAttr,
+    hasAttrValue: hasAttrValue,
+    hasTagName: hasTagName,
+    AND: AND,
+    OR: OR,
+    NOT: NOT
+  }
 };
