df05ef3ccdc30c6570e0d512dab4cbee8457a668
Aleks Totic <a@totic.org>
2015-06-03 14:36:24 -0700

    code review fixes

--- src/start_server.js
+++ src/start_server.js
@@ -15,6 +15,24 @@ var open = require('open');
 var util = require('util');
 var findPort = require('find-port');
 
+function startServer(options) {
+  if (!options.port) {
+    findPort(8080, 8180, function(ports) {
+      options.port = ports[0];
+      startWithPort(options);
+    });
+  }
+  else {
+    startWithPort(options);
+  }
+}
+
+/**
+ * @param {Object} options
+ * @param {Number} options.port -- port number
+ * @param {String=} options.page -- page path, ex: "/", "/index.html"
+ * @param {(String|String[])} options.browser -- names of browser apps to launch
+ */
 function startWithPort(options) {
 
   options.port = options.port || 8080;
@@ -32,7 +50,8 @@ function startWithPort(options) {
 
   server.on('error', function(err) {
     if (err.code === 'EADDRINUSE')
-      console.error("ERROR: Port in use", options.port, "\nPlease choose another port, or let an unused port be chosen automatically.");
+      console.error("ERROR: Port in use", options.port,
+        "\nPlease choose another port, or let an unused port be chosen automatically.");
     process.exit(69);
   });
 
@@ -43,24 +62,12 @@ function startWithPort(options) {
   if (options.page) {
     var url = baseUrl + (options.page === true ? 'index.html' : options.page);
     if (Array.isArray(options.browser)) {
-      for (var i=0; i<options.browser.length; i++)
+      for (var i = 0; i < options.browser.length; i++)
         open(url, options.browser[i]);
     }
     else {
       open(url, options.browser);
     }
-  }
-}
-
-function startServer(options) {
-  if (!options.port) {
-    findPort(8080, 8180, function(ports) {
-      options.port = ports[0];
-      startWithPort(options);
-    });
-  }
-  else {
-    startWithPort(options);
   }
 }
 
