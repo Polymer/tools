07ce3b871cd98aae6563a6a19c3bb7454c9d2872
Daniel Freedman <dfreedm@google.com>
2016-01-08 12:59:41 -0800

    update sauce connect to 4.3.13

--- packages/wct-sauce/lib/sauce.js
+++ packages/wct-sauce/lib/sauce.js
@@ -88,7 +88,7 @@ function isTravisSauceConnectRunning() {
 // sauce-connect-launcher only checks the environment variable
 // $SAUCE_CONNECT_VERSION
 function setSauceConnectDownloadVersion() {
-  process.env.SAUCE_CONNECT_VERSION = '4.3.12';
+  process.env.SAUCE_CONNECT_VERSION = '4.3.13';
 }
 
 module.exports = {
