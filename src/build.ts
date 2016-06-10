6cff2f3d3909316c43894f71ab75a512eda277b3
Justin Fagnani <justinfagnani@google.com>
2016-06-10 13:53:01 -0700

    Initial move from polymer-cli.
    Only minimal changes to make tests and lint pass.

---
+++ src/build.ts
@@ -0,0 +1,221 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+import clone = require('clone');
+import * as fs from 'fs';
+import * as gulp from 'gulp';
+import * as gulpif from 'gulp-if';
+import * as gutil from 'gulp-util';
+import mergeStream = require('merge-stream');
+import * as path from 'path';
+import {PassThrough, Readable} from 'stream';
+import File = require('vinyl');
+import * as vfs from 'vinyl-fs';
+import * as findup from 'findup';
+import * as logging from 'plylog';
+
+import {StreamAnalyzer, DepsIndex} from './analyzer';
+import {Bundler} from './bundle';
+import {ProjectConfig} from '../project-config';
+import {HtmlProject} from './html-project';
+import {optimize, OptimizeOptions} from './optimize';
+import {PrefetchTransform} from './prefetch';
+import {waitForAll, compose, ForkedVinylStream} from './streams';
+import {generateServiceWorker, parsePreCacheConfig, SWConfig} from './sw-precache';
+
+// non-ES compatible modules
+const minimatchAll = require('minimatch-all');
+let logger = logging.getLogger('cli.build.build');
+
+function getGulpfile(): any {
+  // TODO: Should we really be searching procses.cwd()? What about config.root?
+  let foundGulpfileDir;
+  try {
+    foundGulpfileDir = findup.sync(process.cwd(), 'gulpfile.js');
+  } catch (e) {
+    logger.debug(`no gulpfile found (searched up from ${process.cwd()})`);
+    return null;
+  }
+
+  let gulpfulePath = path.join(foundGulpfileDir, 'gulpfile.js');
+  logger.debug('found gulpfile', { path: gulpfulePath });
+  return require(gulpfulePath);
+}
+
+export interface BuildOptions extends OptimizeOptions {
+  sources?: string[];
+  dependencies?: string[];
+  swPrecacheConfig?: string;
+  insertDependencyLinks?: boolean;
+}
+
+export function build(options?: BuildOptions, config?: ProjectConfig): Promise<any> {
+  return new Promise<any>((buildResolve, _) => {
+    options = options || {};
+    let root = config.root;
+    let entrypoint = config.entrypoint;
+    let shell = config.shell;
+    let fragments = config.fragments;
+    let swPrecacheConfig = path.resolve(
+        root, options.swPrecacheConfig || 'sw-precache-config.js');
+    let sources = (options.sources || ['**/*'])
+        .map((p) => path.resolve(root, p));
+    let dependencies = (options.dependencies || ['bower_components/**/*'])
+        .map((p) => path.resolve(root, p));
+    let sourceExcludes = [
+      '!build',
+      '!build/**/*',
+      '!bower_components',
+      '!bower_components/**/*',
+      '!node_modules',
+      '!node_modules/**/*',
+    ];
+
+    logger.debug(`shell: ${shell}`);
+    logger.debug(`entrypoint: ${entrypoint}`);
+    logger.debug(`dependencies: ${dependencies}`);
+    if (options.insertDependencyLinks) {
+      logger.debug(`Additional dependency links will be inserted into application`);
+    }
+
+    let allSources = [];
+    allSources.push(entrypoint);
+    if (shell) allSources.push(shell);
+    allSources = allSources.concat(fragments, sources, sourceExcludes);
+    logger.debug(`sources: ${allSources}`);
+
+    let allFragments = [];
+    if (shell) allFragments.push(shell);
+    allFragments = allFragments.concat(fragments);
+    logger.debug(`fragments: ${fragments}`);
+
+    // TODO: let this be set by the user
+    let optimizeOptions: OptimizeOptions = {
+      html: {
+        removeComments: true,
+      },
+      css: {
+        stripWhitespace: true
+      },
+      js: {
+        minify: true
+      }
+    };
+
+    // mix in optimization options from build command
+    if (options.html) {
+      Object.assign(optimizeOptions.html, options.html);
+    }
+    if (options.css) {
+      Object.assign(optimizeOptions.css, options.css);
+    }
+    if (options.js) {
+      Object.assign(optimizeOptions.js, options.js);
+    }
+
+    let gulpfile = getGulpfile();
+    let userTransformers = gulpfile && gulpfile.transformers;
+    if (userTransformers) {
+      logger.debug(`${userTransformers.length} transformers found in gulpfile`);
+    }
+
+    let sourcesProject = new HtmlProject();
+    let depsProject = new HtmlProject();
+    let analyzer = new StreamAnalyzer(root, entrypoint, shell, fragments);
+    let bundler = new Bundler(root, entrypoint, shell, fragments, analyzer);
+
+    logger.info(`Building application...`);
+    logger.debug(`Reading source files...`);
+    let sourcesStream =
+      vfs.src(allSources, {cwdbase: true, allowEmpty: true})
+        .pipe(sourcesProject.split)
+        .pipe(compose(userTransformers))
+        .pipe(optimize(optimizeOptions))
+        .pipe(sourcesProject.rejoin);
+
+    logger.debug(`Reading dependencies...`);
+    let depsStream =
+      vfs.src(dependencies, {cwdbase: true, allowEmpty: true})
+        .pipe(depsProject.split)
+        .pipe(optimize(optimizeOptions))
+        .pipe(depsProject.rejoin);
+
+    let allFiles = mergeStream(sourcesStream, depsStream)
+      .once('data', () => { logger.debug('Analyzing build dependencies...'); })
+      .pipe(analyzer);
+
+    let serviceWorkerName = 'service-worker.js';
+
+    let unbundledPhase = new ForkedVinylStream(allFiles)
+      .once('data', () => { logger.info('Generating build/unbundled...'); })
+      .pipe(
+        gulpif(
+          options.insertDependencyLinks,
+          new PrefetchTransform(root, entrypoint,
+                shell, fragments, analyzer)
+        )
+      )
+      .pipe(vfs.dest('build/unbundled'));
+
+    let bundledPhase = new ForkedVinylStream(allFiles)
+      .once('data', () => { logger.info('Generating build/bundled...'); })
+      .pipe(bundler)
+      .pipe(vfs.dest('build/bundled'));
+
+    let genSW = (buildRoot: string, deps: string[], swConfig: SWConfig, scriptAndStyleDeps?: string[]) => {
+      logger.debug(`Generating service worker for ${buildRoot}...`);
+      logger.debug(`Script and style deps: ${scriptAndStyleDeps}`);
+      return generateServiceWorker({
+        root,
+        entrypoint,
+        deps,
+        scriptAndStyleDeps,
+        buildRoot,
+        swConfig: clone(swConfig),
+        serviceWorkerPath: path.join(root, buildRoot, serviceWorkerName)
+      });
+    };
+
+    waitForAll([unbundledPhase, bundledPhase])
+      .then(() => analyzer.analyze)
+      .then((depsIndex) => {
+        let unbundledDeps = analyzer.allFragments
+            .concat(Array.from(depsIndex.depsToFragments.keys()));
+
+        let fullDeps = Array.from(depsIndex.fragmentToFullDeps.values());
+        let scriptAndStyleDeps = new Set<string>();
+        fullDeps.forEach(d => {
+          d.scripts.forEach((s) => scriptAndStyleDeps.add(s));
+          d.styles.forEach((s) => scriptAndStyleDeps.add(s));
+        });
+
+        let bundledDeps = analyzer.allFragments
+            .concat(bundler.sharedBundleUrl);
+
+        parsePreCacheConfig(swPrecacheConfig).then((swConfig) => {
+          if (swConfig) {
+            logger.debug(`Service worker config found`, swConfig);
+          } else {
+            logger.debug(`No service worker configuration found at ${swPrecacheConfig}, continuing with defaults`);
+          }
+
+          logger.info(`Generating service workers...`);
+          return Promise.all([
+            genSW('build/unbundled', unbundledDeps, swConfig, Array.from(scriptAndStyleDeps)),
+            genSW('build/bundled', bundledDeps, swConfig)
+          ]);
+        })
+        .then(() => {
+          logger.info('Build complete!');
+          buildResolve();
+        });
+      });
+  });
+}
---
+++ src/prefetch.ts
@@ -0,0 +1,154 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+import * as dom5 from 'dom5';
+import * as path from 'path';
+import * as logging from 'plylog';
+import {Transform} from 'stream';
+import File = require('vinyl');
+
+import {StreamAnalyzer, DepsIndex} from './analyzer';
+
+let logger = logging.getLogger('cli.build.prefech');
+
+export class PrefetchTransform extends Transform {
+  root: string;
+  entrypoint: string;
+  shell: string;
+  fragments: string[];
+  allFragments: string[];
+  fileMap: Map<string, File>;
+  analyzer: StreamAnalyzer;
+
+  constructor(
+    /**
+     * Root of the dependencies.
+     * Will be stripped when making links
+     */
+    root: string,
+
+    /**
+     * The main HTML file. This will have link rel=prefetches added to it.
+     */
+    entrypoint: string,
+
+    /**
+     * The app shell. This will have link rel=imports added to it.
+     */
+    shell: string,
+
+    /**
+     * List of files that will have dependencies flattened with
+     * `<link rel="import">`
+     */
+    fragments: string[],
+
+    /**
+     * The analyzer to retreive dependency information from.
+     */
+    analyzer: StreamAnalyzer
+  ) {
+    super({objectMode: true});
+    this.root = root;
+    this.entrypoint = entrypoint;
+    this.shell = shell;
+    this.fragments = fragments;
+    // clone fragments
+    this.allFragments = Array.from(fragments);
+    if (shell) {
+      this.allFragments.push(shell);
+    } else {
+      this.allFragments.push(entrypoint);
+    }
+    this.analyzer = analyzer;
+    this.fileMap = new Map<string, File>();
+  }
+
+  pullUpDeps(
+    file: File,
+    deps: string[],
+    type: 'import' | 'prefetch'
+  ) {
+    let contents = file.contents.toString();
+    let ast = dom5.parse(contents);
+    let head = dom5.query(ast, dom5.predicates.hasTagName('head'));
+    for (let dep of deps) {
+      if (dep.startsWith(this.root)) {
+        dep = path.relative(file.dirname, dep);
+      }
+      // prefetched deps should be absolute, as they will be in the main file
+      if (type === 'prefetch') {
+        dep = path.join('/', dep);
+      }
+      let link = dom5.constructors.element('link');
+      dom5.setAttribute(link, 'rel', type);
+      dom5.setAttribute(link, 'href', dep);
+      dom5.append(head, link);
+    }
+    contents = dom5.serialize(ast);
+    file.contents = new Buffer(contents);
+  }
+
+  _transform(file: File, enc: string, callback: (err?, file?) => void) {
+    if (this.isImportantFile(file)) {
+      // hold on to the file for safe keeping
+      this.fileMap.set(file.path, file);
+      callback(null, null);
+    } else {
+      callback(null, file);
+    }
+  }
+
+  isImportantFile(file) {
+    return file.path === this.entrypoint ||
+        this.allFragments.indexOf(file.path) > -1;
+  }
+
+  _flush(done: (err?) => void) {
+    if (this.fileMap.size === 0) {
+      return done();
+    }
+    this.analyzer.analyze.then((depsIndex: DepsIndex) => {
+      let fragmentToDeps = new Map(depsIndex.fragmentToDeps);
+
+      if (this.entrypoint && this.shell) {
+        let file = this.fileMap.get(this.entrypoint);
+        // forward shell's dependencies to main to be prefetched
+        let deps = fragmentToDeps.get(this.shell);
+        if (deps) {
+          this.pullUpDeps(file, deps, 'prefetch');
+        }
+        this.push(file);
+        this.fileMap.delete(this.entrypoint);
+      }
+
+      for (let im of this.allFragments) {
+        let file = this.fileMap.get(im);
+        let deps = fragmentToDeps.get(im);
+        if (deps) {
+          this.pullUpDeps(file, deps, 'import');
+        }
+        this.push(file);
+        this.fileMap.delete(im);
+      }
+
+      for (let leftover of this.fileMap.keys()) {
+        logger.warn(
+          'File was listed in fragments but not found in stream:',
+          leftover
+        );
+        this.push(this.fileMap.get(leftover));
+        this.fileMap.delete(leftover);
+      }
+
+      done();
+    });
+  }
+}
