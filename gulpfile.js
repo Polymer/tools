ed1dd8f508791c8f005efd272dc283e1904cf829
Peter Burns <rictic@gmail.com>
2016-07-21 18:55:41 -0700

    Make it possible to use tools-common's gulp tasks as a library (#5)
    * More work to make tools-common librarified
    
    * Add version to package.json
    
    * Use cwd rather than __dirname
    
    * Use the tools-common .eslintrc.json if no project-specific one is found
    
    * Use tools-common tslint as a fallback too
    
    * Update gulp-typings
    
    * Document the "if main" section of gulpfile.js
    
    * Document the subtle gulp cwd constraint
    
    * Gulp as peer dependency, gulp tasks as gulp-tasks.js
    
    * Add generateCompleteTaskgraph, fix style nits and improve docs

--- gulpfile.js
+++ gulpfile.js
@@ -10,134 +10,20 @@
 
 'use strict';
 
-const depcheck_lib = require('depcheck');
-const eslint_lib = require('gulp-eslint');
-const fs = require('fs-extra');
 const gulp = require('gulp');
-const mergeStream = require('merge-stream');
-const mocha = require('gulp-mocha');
-const path = require('path');
-const runSeq = require('run-sequence');
-const tslint_lib = require("gulp-tslint");
-const typescript = require('gulp-typescript');
-const typings = require('gulp-typings');
+const toolsCommon = require('./gulp-tasks');
+// Use this instead when you copy-paste this file into other repos:
+// const toolsCommon = require('tools-common/gulp-tasks');
 
-function task(name, deps, impl) {
-  if (gulp.hasTask(name)) {
-    throw new Error(
-        `A task with the name ${JSON.stringify(name)} already exists!`);
-  }
-  gulp.task(name, deps, impl);
-}
+toolsCommon.generateCompleteTaskgraph({
+  // What will get deleted when gulp clean is run
+  buildArtifacts: ['lib/', 'typings/'],
 
-module.exports.init = function() {
-  task('init', () => gulp.src("./typings.json").pipe(typings()));
-}
+  // Dependencies that we assert are actually used, even if they look unused.
+  stickyDeps: new Set([/* sticky deps here */]),
 
-module.exports.depcheck = function depcheck(options) {
-  const defaultOptions = {stickyDeps: new Set()};
-  options = Object.assign({}, defaultOptions, options);
-
-  task('depcheck', () => {
-    return new Promise((resolve, reject) => {
-      depcheck_lib(__dirname, {ignoreDirs: []}, resolve);
-    }).then((result) => {
-      const invalidFiles = Object.keys(result.invalidFiles) || [];
-      const invalidJsFiles = invalidFiles.filter((f) => f.endsWith('.js'));
-
-      if (invalidJsFiles.length > 0) {
-        console.log('Invalid files:', result.invalidFiles);
-        throw new Error('Invalid files');
-      }
-
-      const unused = new Set(result.dependencies);
-      for (const falseUnused of options.stickyDeps) {
-        unused.delete(falseUnused);
-      }
-      if (unused.size > 0) {
-        console.log('Unused dependencies:', unused);
-        throw new Error('Unused dependencies');
-      }
-    });
-  });
-}
-
-module.exports.lint = function(options) {
-  module.exports.tslint(options);
-  module.exports.eslint_lib(options);
-  module.exports.depcheck(options);
-  task('lint', ['tslint', 'eslint', 'depcheck']);
-}
-
-module.exports.tslint = function(options) {
-  const defaultOptions = {tsSrcs: gulp.src('src/**/*.ts')};
-  options = Object.assign({}, defaultOptions, options);
-  task('tslint', () =>
-      options.tsSrcs
-        .pipe(tslint_lib({
-          configuration: 'tslint.json',
-        }))
-        .pipe(tslint_lib.report('verbose')));
-}
-
-module.exports.eslint = function(options) {
-  const defaultOptions = {jsSrcs: gulp.src('test/**/*.js')};
-  options = Object.assign({}, defaultOptions, options);
-  task('eslint', () =>
-      options.jsSrcs
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError()));
-}
-
-module.exports.build = function(options) {
-  const defaultOptions = {
-    tsSrcs: gulp.src('src/**/*.ts'),
-    dataSrcs: gulp.src(['src/**/*', '!src/**/*.ts'])
-  };
-  options = Object.assign({}, defaultOptions, options);
-
-  const tsProject = typescript.createProject('tsconfig.json');
-
-  task('build', () =>
-    mergeStream(
-      options.tsSrcs.pipe(typescript(tsProject)),
-      options.dataSrcs
-    ).pipe(gulp.dest('lib'))
-  );
-}
-
-module.exports.clean = function(options) {
-  const defaultOptions = {buildArtifacts: ['lib']};
-  options = Object.assign({}, defaultOptions, options);
-
-  task('clean', () => {
-    for (const buildArtifact of options.buildArtifacts) {
-      fs.removeSync(path.join(__dirname, buildArtifact));
-    }
-  });
-}
-
-
-module.exports.buildAll = function(options) {
-  module.exports.clean(options);
-  module.exports.init(options);
-  module.exports.lint(options);
-  module.exports.build(options);
-
-  task('build-all', (done) => {
-    runSeq('clean', 'init', 'lint', 'build', done);
-  });
-}
-
-module.exports.test = function(options) {
-  module.exports.buildAll(options);
-
-  task('test', ['build'], () =>
-    gulp.src('test/**/*_test.js', {read: false})
-        .pipe(mocha({
-          ui: 'tdd',
-          reporter: 'spec',
-        }))
-  );
-}
+  // Typescript sources here
+  tsSrcs: gulp.src('src/**/*.ts'),
+  // Files to copy to lib/
+  dataSrcs: gulp.src(['src/**/*', '!src/**/*.ts']),
+});
