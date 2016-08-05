f41edac57b9a984fc59889a90c8e3b363f40a516
Peter Burns <rictic@gmail.com>
2016-08-05 14:27:12 -0700

    Noop change: fixup project settings (#37)
    * Get wct-local compiling cleanly
    
    * Use upstream typing for which
    
    * Clean up project settings a bit

---
+++ packages/wct-local/custom_typings/main.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="launchpad.d.ts" />
+/// <reference path="promisify-node.d.ts" />
+/// <reference path="selenium-standalone.d.ts" />
+/// <reference path="wct.d.ts" />
+/// <reference path="wd.d.ts" />
--- packages/wct-local/custom_typings/which.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'which' {
-  function which(binName: string, cb: (error?: Error, path?: string) => void): void;
-  module which {
-    function sync(name: string): string;
-  }
-  export = which;
-}
