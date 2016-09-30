df3875ee6e58abfe18473877296ca7e66ef510e6
Peter Burns <rictic@google.com>
2016-09-29 17:06:05 -0700

    Initial import of the editor service from polymer-analyzer

---
+++ src/polymer-editor-server.ts
@@ -0,0 +1,113 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+/**
+ * This file is a binary not a library, and should be run via
+ * `node polymer-editor-server.js` or `child_process.fork`
+ *
+ * Communication with this server is done via the Remote Editor Protocol via
+ * stdin and stdout, as well as the node process.on('message') IPC process.
+ *
+ * See 'remote-editor-protocol.ts' for details on the communication protocol.
+ */
+
+import split = require('split');
+import * as util from 'util';
+
+import {FSUrlLoader} from 'polymer-analyzer/lib/url-loader/fs-url-loader';
+import {PackageUrlResolver} from 'polymer-analyzer/lib/url-loader/package-url-resolver';
+
+import {LocalEditorService} from './local-editor-service';
+import {Request, RequestWrapper, ResponseWrapper, SettledValue} from './remote-editor-protocol';
+
+
+/**
+ * Handles decoded Requests, dispatching them to a local editor service.
+ */
+class EditorServer {
+  private _localEditorService: LocalEditorService;
+
+  async handleMessage(message: Request): Promise<any> {
+    if (message.kind === 'init') {
+      if (this._localEditorService) {
+        throw new Error('Already initialized!');
+      }
+      this._localEditorService = new LocalEditorService({
+        urlLoader: new FSUrlLoader(message.basedir),
+        urlResolver: new PackageUrlResolver()
+      });
+      return;
+    }
+    const localEditorService = this._localEditorService;
+    if (!localEditorService) {
+      throw new Error(
+          `Must send an 'init' message before any others. ` +
+          `Received '${message.kind}' message before 'init'.`);
+    }
+    switch (message.kind) {
+      case 'getWarningsFor':
+        return localEditorService.getWarningsForFile(message.localPath);
+      case 'fileChanged':
+        await localEditorService.fileChanged(
+            message.localPath, message.contents);
+        return;
+      case 'getDefinitionFor':
+        return localEditorService.getDefinitionForFeatureAtPosition(
+            message.localPath, message.position);
+      case 'getDocumentationFor':
+        return localEditorService.getDocumentationAtPosition(
+            message.localPath, message.position);
+      case 'getTypeaheadCompletionsFor':
+        return localEditorService.getTypeaheadCompletionsAtPosition(
+            message.localPath, message.position);
+      case '_clearCaches':
+        return localEditorService._clearCaches();
+      default:
+        const never: never = message;
+        throw new Error(`Got unknown kind of message: ${util.inspect(never)}`);
+    }
+  }
+}
+
+
+const server: EditorServer = new EditorServer();
+
+// stdin/stdout interface
+process.stdin.setEncoding('utf8');
+process.stdin.resume();
+process.stdin.pipe(split()).on('data', async function(line: string) {
+  const request: RequestWrapper = JSON.parse(line);
+  const result = await getSettledValue(request.value);
+  process.stdout.write(
+      JSON.stringify(<ResponseWrapper>{id: request.id, value: result}) + '\n');
+});
+
+// node child_process.fork() IPC interface
+process.on('message', async function(request: RequestWrapper) {
+  const result = await getSettledValue(request.value);
+  process.send!(<ResponseWrapper>{id: request.id, value: result});
+});
+
+
+/**
+ * Calls into the server and converts its responses into SettledValues.
+ */
+async function getSettledValue(request: Request): Promise<SettledValue> {
+  try {
+    const value = await server.handleMessage(request);
+    return {kind: 'resolution', resolution: value};
+  } catch (e) {
+    return {kind: 'rejection', rejection: e.message || e.stack || e.toString()};
+  }
+}
---
+++ src/remote-editor-protocol.ts
@@ -0,0 +1,83 @@
+/**
+ * @license
+ * Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import {SourcePosition} from './editor-service';
+
+/**
+ * The remote editor service protocol is request/response based.
+ * Requests and responses are JSON serialized objects.
+ *
+ * Every request must have a unique identifier. For every request there
+ * will be at most one response with the corresponding identifier. Responses
+ * may come in any order.
+ *
+ * Responses are modeled on settled Promise values. A response is either
+ * successful and resolved or unsuccessful and rejected.
+ *
+ * The types of requests and responses obey the EditorService interface, with
+ * some modification of method calls for the JSON format. See the Request
+ * type for more information.
+ */
+export interface RequestWrapper {
+  id: number;
+  value: Request;
+}
+export interface ResponseWrapper {
+  id: number;
+  value: SettledValue;
+}
+export type SettledValue = Resolution | Rejection;
+export interface Resolution {
+  kind: 'resolution';
+  resolution: any;
+}
+export interface Rejection {
+  kind: 'rejection';
+  rejection: string;
+}
+
+
+export type Request = InitRequest | FileChangedRequest | GetWarningsRequest |
+    GetDocumentationRequest | GetDefinitionRequest |
+    GetTypeaheadCompletionsRequest | ClearCachesRequest;
+export interface InitRequest {
+  kind: 'init';
+  basedir: string;
+}
+export interface FileChangedRequest {
+  kind: 'fileChanged';
+  localPath: string;
+  contents?: string;
+}
+export interface GetWarningsRequest {
+  kind: 'getWarningsFor';
+  localPath: string;
+}
+export interface GetDocumentationRequest {
+  kind: 'getDocumentationFor';
+  localPath: string;
+  position: SourcePosition;
+}
+export interface GetDefinitionRequest {
+  kind: 'getDefinitionFor';
+  localPath: string;
+  position: SourcePosition;
+}
+export interface GetTypeaheadCompletionsRequest {
+  kind: 'getTypeaheadCompletionsFor';
+  localPath: string;
+  position: SourcePosition;
+}
+/** Internal, don't use this. May break in the future. */
+export interface ClearCachesRequest { kind: '_clearCaches'; }
