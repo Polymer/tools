877f043c5142129dbd2ed7436efaabc0a00d14d3
Fred K. Schott <fkschott@gmail.com>
2016-11-03 10:19:08 -0700

    export missing typedefs (#73)

--- src/polymer-build.ts
+++ src/polymer-build.ts
@@ -1,3 +1,9 @@
 export {PolymerProject} from './polymer-project';
 export {forkStream} from './fork-stream';
 export {generateServiceWorker, addServiceWorker} from './service-worker';
+
+// Export types for upstream TypeScript projects
+export {StreamAnalyzer, DepsIndex} from './analyzer';
+export {Bundler} from './bundle';
+export {AddServiceWorkerOptions} from './service-worker';
+export {SWConfig} from 'sw-precache';
