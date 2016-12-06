481adc9b72371d97d1d2a9bebb61ecfd9b950876
Justin Fagnani <justinfagnani@google.com>
2016-12-05 21:34:49 -0800

    Fix compilation errors

--- packages/wct-local/custom_typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="launchpad.d.ts" />
-/// <reference path="promisify-node.d.ts" />
-/// <reference path="selenium-standalone.d.ts" />
-/// <reference path="wct.d.ts" />
-/// <reference path="wd.d.ts" />
