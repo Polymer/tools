acec77c8487f80c85c8f11cfc6d62c2f4ced16dc
Peter Burns <rictic@gmail.com>
2017-02-21 15:02:10 -0800

    Use upstream memory-streams typing. (#466)

--- custom_typings/main.d.ts
+++ custom_typings/main.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="./chai.d.ts" />
 /// <reference path="./espree.d.ts" />
-/// <reference path="./memory-streams.d.ts" />
 /// <reference path="./performance-now.d.ts" />
 /// <reference path="./shady-css-parser.d.ts" />
 /// <reference path="./strip-indent.d.ts" />
