0dd39d2e0bbbf943e0ec3a2e75818eaa94bacb5c
Justin Fagnani <justinfagnani@google.com>
2017-06-20 09:47:25 -0700

    Exclude references after rewriting exports to support exporting excluded references.
    Fixes #35

--- src/test/analysis-converter_test.ts
+++ src/test/analysis-converter_test.ts
@@ -627,6 +627,24 @@ class MyElement extends $Element {}\n`);
       assert.equal(js, `if (undefined) {}\n`);
     });
 
+    test('handles excluded exported references', async () => {
+      setSources({
+        'test.html': `
+          <script>
+            Polymer.Settings = settings;
+          </script>
+        `,
+      });
+      const analysis = await analyzer.analyze(['test.html']);
+      const converter = new AnalysisConverter(analysis, {
+        rootModuleName: 'Polymer',
+        referenceExcludes: ['Polymer.Settings'],
+      });
+      const converted = await converter.convert();
+      const js = converted.get('./test.js');
+      assert.equal(js, `export { settings as Settings };\n`);
+    });
+
     test('inlines templates into class-based Polymer elements', async () => {
       setSources({
         'test.html': `
