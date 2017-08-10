3c6f6476b86f6afd9fee4c28ce48d52b1f1257fd
Alexander Marks <aomarks@google.com>
2017-08-10 12:17:21 -0700

    Use new browser-capabilities library to decide whether to compile.

--- src/compile-middleware.ts
+++ src/compile-middleware.ts
@@ -13,12 +13,12 @@
  */
 
 import * as babelCore from 'babel-core';
+import {browserCapabilities} from 'browser-capabilities';
 import {parse as parseContentType} from 'content-type';
 import * as dom5 from 'dom5';
 import {Request, RequestHandler, Response} from 'express';
 import * as LRU from 'lru-cache';
 import * as parse5 from 'parse5';
-import {UAParser} from 'ua-parser-js';
 
 import {transformResponse} from './transform-middleware';
 
@@ -135,25 +135,5 @@ const isInlineJavaScript = dom5.predicates.AND(
     dom5.predicates.NOT(dom5.predicates.hasAttr('src')));
 
 export function browserNeedsCompilation(userAgent: string): boolean {
-  const browser = new UAParser(userAgent).getBrowser();
-  const versionSplit = (browser.version || '').split('.');
-  const [majorVersion, minorVersion] =
-      versionSplit.map((v) => v ? parseInt(v, 10) : -1);
-
-  const supportsES2015 = (browser.name === 'Chrome' && majorVersion >= 49) ||
-      (browser.name === 'Chromium' && majorVersion >= 49) ||
-      (browser.name === 'OPR' && majorVersion >= 36) ||
-      (browser.name === 'Vivaldi' && majorVersion >= 1) ||
-      (browser.name === 'Mobile Safari' && majorVersion >= 10) ||
-      (browser.name === 'Safari' && majorVersion >= 10) ||
-      // Note: The Edge user agent uses the EdgeHTML version, not the main
-      // release version (e.g. EdgeHTML 15 corresponds to Edge 40). See
-      // https://en.wikipedia.org/wiki/Microsoft_Edge#Release_history.
-      //
-      // Versions before 15.15063 may contain a JIT bug affecting ES6
-      // constructors (see #161).
-      (browser.name === 'Edge' &&
-       (majorVersion > 15 || (majorVersion === 15 && minorVersion >= 15063))) ||
-      (browser.name === 'Firefox' && majorVersion >= 51);
-  return !supportsES2015;
+  return !browserCapabilities(userAgent).has('es2015');
 }
