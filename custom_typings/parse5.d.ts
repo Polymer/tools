ff769cfb218a72ba67fcb58cde219d588c5bd298
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2017-08-28 12:30:27 -0700

    Update parse5 typings to remove custom_typings (#57)

--- custom_typings/parse5.d.ts
+++
@@ -1,7 +0,0 @@
-import {ASTNode} from 'parse5';
-
-declare module 'parse5' {
-  // TODO(fks) 11-01-2016: Remove this once @types/parse5 includes `tagName`
-  // (https://github.com/DefinitelyTyped/DefinitelyTyped/pull/12424)
-  export interface ASTNode { tagName?: string; }
-}
