05407e7a7dbd9621f0e9ed050c380f65e37e8e27
Peter Burns <rictic@gmail.com>
2017-12-18 15:12:37 -0800

    Add --help and --version CLI options. (#109)

--- src/polymer-language-server.ts
+++ src/polymer-language-server.ts
@@ -12,18 +12,47 @@
  *
  * [1]: https://github.com/Microsoft/language-server-protocol
  */
+async function main() {
+  // This import *must* come before all others.
+  await import('./intercept-logs');
+  //
 
-// This import *must* come before all others.
-import './intercept-logs';
-//
+  const {createConnection} = await import('vscode-languageserver');
+  const {default: LanguageServer} =
+      await import('./language-server/language-server');
 
-import {createConnection, IConnection} from 'vscode-languageserver';
-import LanguageServer from './language-server/language-server';
+  // Create a connection for the server. Communicate using stdio.
+  let connection = createConnection(process.stdin, process.stdout);
 
-// Create a connection for the server. Communicate using stdio.
-let connection: IConnection = createConnection(process.stdin, process.stdout);
+  const serverPromise = LanguageServer.initializeWithConnection(connection);
 
+  connection.listen();
 
-LanguageServer.initializeWithConnection(connection);
+  await serverPromise;
+}
 
-connection.listen();
+const version = require('../package.json').version;
+if (process.argv.includes('--help')) {
+  console.log(`Usage: polymer-editor-service
+
+    Polymer Editor Service v${version}
+
+    Speaks the Language Server Protocol over stdin/stdout.
+    Hook it up to your editor, and configure it for HTML,
+    JS, CSS, and JSON files.
+
+    More info at https://github.com/Polymer/polymer-editor-service
+
+Options:
+  --help:     print this message
+  --version:  print version
+`);
+} else if (process.argv.includes('--version')) {
+  console.log(`Polymer Editor Service v${version}`);
+} else {
+  main().catch((err) => {
+    console.error(`Failed to initialize polymer editor service:`);
+    console.error(err);
+    process.exitCode = 1;
+  });
+}
