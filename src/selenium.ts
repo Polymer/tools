9fad8638763da5b09d34cdc9f320b01e11c4b735
Daniel Freedman <compismyrx@gmail.com>
2018-01-12 12:35:30 -0800

    Update dependencies (#72)
    * Update dependencies
    
    Update selenium-standalone for latest browser drivers
    Update chalk
    
    * Fix optional typing for browser options

--- src/selenium.ts
+++ src/selenium.ts
@@ -7,7 +7,7 @@
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
-import * as chalk from 'chalk';
+import chalk from 'chalk';
 import * as cleankill from 'cleankill';
 import * as freeport from 'freeport';
 import * as selenium from 'selenium-standalone';
