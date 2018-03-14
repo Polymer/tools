6f3c813e00b4aa88c125c4b094a945106e775c09
Daniel Freedman <dfreedm@google.com>
2018-03-14 12:37:05 -0700

    Do not override, only modify
    - Check for and remove all references to `bower`
    - Add `--npm` flags to `wct` and `polymer test`
    - Do not force `yarn`
    - Update fixtures

--- src/travis-config.ts
+++ src/travis-config.ts
@@ -13,17 +13,52 @@
  */
 
 import * as fse from 'fs-extra';
-import {safeDump, safeLoad} from 'js-yaml';
 import * as path from 'path';
 
-const TRAVIS_CONFIG = '.travis.yml';
+import { safeDump, safeLoad } from 'js-yaml';
+
+const travisConfigFile = '.travis.yml';
 
 // https://docs.travis-ci.com/user/languages/javascript-with-nodejs/
 interface TravisConfig {
-  before_script?: string|string[];
-  install?: string|string[];
-  script?: string|string[];
-  cache?: string|Array<string|{directories: string[]}>;
+  before_script?: string | string[];
+  install?: string | string[];
+  script?: string | string[];
+  cache?: string | Array<string | { directories: string[] } | { [key: string]: boolean }>;
+}
+
+function addNPMFlag(scripts: string[]): string[] {
+  return scripts.map((script) => {
+    if (script.indexOf('polymer test') > -1 && script.indexOf('--npm') === -1) {
+      script = script.replace('polymer test', 'polymer-test --npm');
+    }
+    if (script.indexOf('wct') > -1 && script.indexOf('--npm') === -1) {
+      script = script.replace('wct', 'wct --npm');
+    }
+    return script;
+  });
+}
+
+function removeBowerAndPolymerInstall(scripts: string[]): string[] {
+  return scripts.map((script) => {
+    if (script.match(/bower i(?:nstall)?/) || script.indexOf('polymer install') > -1) {
+      return '';
+    }
+    if (script.match(/npm i(?:install)?|yarn add/)) {
+      return script.split(' ').filter((s) => s !== 'bower').join(' ');
+    }
+    return script;
+  }).filter((s) => !!s);
+}
+
+function configToArray(yamlPart: string | string[] | undefined): string[] {
+  if (!yamlPart) {
+    return [];
+  } else if (typeof yamlPart === 'string') {
+    return [yamlPart];
+  } else {
+    return yamlPart;
+  }
 }
 
 /**
@@ -34,26 +69,39 @@ interface TravisConfig {
  * @param outDir Root path of output travis config
  */
 export async function transformTravisConfig(
-    inDir: string, outDir: string): Promise<void> {
-  const inTravisPath = path.join(inDir, TRAVIS_CONFIG);
+  inDir: string, outDir: string): Promise<void> {
+
+  const inTravisPath = path.join(inDir, travisConfigFile);
+
+  if (!await fse.pathExists(inTravisPath)) {
+    return;
+  }
+
   const travisBlob = (await fse.readFile(inTravisPath)).toString();
   const travisConfig = safeLoad(travisBlob) as Partial<TravisConfig>;
 
-  // use 'yarn' to install
-  travisConfig.install = 'yarn';
+  let beforeScript = configToArray(travisConfig.before_script);
+  let testScript = configToArray(travisConfig.script);
+  let installScript = configToArray(travisConfig.install);
 
-  // cache yarn directories
-  travisConfig.cache = 'yarn';
+  // remove use of `bower` and `polymer install`
+  beforeScript = removeBowerAndPolymerInstall(beforeScript);
+  testScript = removeBowerAndPolymerInstall(testScript);
+  installScript = removeBowerAndPolymerInstall(installScript);
 
-  // install polymer-cli
-  travisConfig.before_script = 'npm install -g polymer-cli';
+  // use `--npm` in `polymer test` and `wct` commands
+  testScript = addNPMFlag(testScript);
 
-  // use `--npm` in `polymer test` commands
-  travisConfig.script = [
-    'polymer test --npm',
-    '>-\nif [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then polymer test --npm -s \'default\';\nfi'
-  ];
+  travisConfig.before_script = beforeScript;
+  travisConfig.script = testScript;
+  // only add `install` config if previous config had one
+  if (installScript.length > 0) {
+    travisConfig.install = installScript;
+  } else {
+    delete travisConfig.install;
+  }
 
-  const outPath = path.join(outDir, TRAVIS_CONFIG);
-  await fse.writeFile(outPath, safeDump(travisConfig));
+  const outPath = path.join(outDir, travisConfigFile);
+  const travisBlobOut = safeDump(travisConfig);
+  await fse.writeFile(outPath, travisBlobOut);
 }
