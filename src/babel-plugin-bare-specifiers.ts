d1c0b1b672b448c805ddc161bc3cd8a9849dcc4c
Justin Fagnani <justin@fagnani.com>
2018-03-29 11:09:32 -0700

    Use node module resolution function from Analyzer (#336)

--- src/babel-plugin-bare-specifiers.ts
+++ src/babel-plugin-bare-specifiers.ts
@@ -12,16 +12,9 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import {dirname, join, relative} from 'path';
-
-import resolve = require('resolve');
 import {NodePath} from 'babel-traverse';
-import * as pathlib from 'path';
-import * as isWindows from 'is-windows';
-import * as whatwgUrl from 'whatwg-url';
-import {ImportDeclaration, ExportNamedDeclaration, ExportAllDeclaration} from 'babel-types';
-
-const pathIsInside = require('path-is-inside');
+import {ExportAllDeclaration, ExportNamedDeclaration, ImportDeclaration} from 'babel-types';
+import {resolve} from 'polymer-analyzer/lib/javascript/resolve-specifier-node';
 
 const exportExtensions = require('babel-plugin-syntax-export-extensions');
 
@@ -53,32 +46,16 @@ export const resolveBareSpecifiers = (
 
       const specifier = node.source.value;
 
-      if (whatwgUrl.parseURL(specifier) !== null) {
-        return;
-      }
-
-      let resolvedSpecifier;
       try {
-        resolvedSpecifier = resolve.sync(specifier, {
-          basedir: pathlib.dirname(filePath),
-          // It's invalid to load a .json or .node file as a module on the web,
-          // but this is what Node's resolution algorithm does
-          // (https://nodejs.org/api/modules.html#modules_all_together), so we
-          // also do it here for completeness. Without including these
-          // extensions the user will probably get a 404. With them, they'll
-          // probably get an invalid MIME type error (which is hopefully more
-          // useful).
-          extensions: ['.js', '.json', '.node'],
-          // Some packages use a non-standard alternative to the "main" field
-          // in their package.json to differentiate their ES module version.
-          packageFilter: (
-              packageJson:
-                  {main?: string, module?: string, 'jsnext:main'?: string}) => {
-            packageJson.main = packageJson.module ||
-                packageJson['jsnext:main'] || packageJson.main;
-            return packageJson;
-          },
-        });
+        let componentInfo;
+        if (isComponentRequest) {
+          componentInfo = {
+            packageName: packageName!,
+            componentDir: componentDir!,
+            rootDir: rootDir!,
+          };
+        }
+        node.source.value = resolve(specifier, filePath, componentInfo);
       } catch (e) {
         if (!isPathSpecifier(specifier)) {
           // Don't warn if the specifier was already a path, even though we do
@@ -88,44 +65,6 @@ export const resolveBareSpecifiers = (
         return;
       }
 
-      if (!resolvedSpecifier.endsWith('.js')) {
-        console.warn(
-            `Module specifier "${specifier}" ` +
-            `resolved to non .js file "${resolvedSpecifier}"`);
-      }
-
-      let relativeSpecifierUrl: string;
-      // If we have an import that crosses the top-level / componentDir
-      // boundary, fix up the path to generate polyserve-style ../ cross-package
-      // imports.
-      if (isComponentRequest === true && componentDir !== undefined &&
-          rootDir !== undefined &&
-          pathIsInside(resolvedSpecifier, componentDir) &&
-          !pathIsInside(filePath, componentDir)) {
-        if (packageName === undefined) {
-          throw new Error(
-              'resolveBareSpecifiers(): packageName must be set when isComponentRequest === true');
-        }
-        const relativePathFromComponentDir =
-            relative(componentDir, resolvedSpecifier);
-        // Important: Create a URL pointing up from the root dir
-        const relativeDistance =
-            packageName!.startsWith('@') ? '../../' : '../';
-        relativeSpecifierUrl = relative(
-            dirname(filePath),
-            join(rootDir, relativeDistance, relativePathFromComponentDir));
-      } else {
-        relativeSpecifierUrl = relative(dirname(filePath), resolvedSpecifier);
-      }
-
-      if (isWindows()) {
-        relativeSpecifierUrl = relativeSpecifierUrl.replace(/\\/g, '/');
-      }
-
-      if (!isPathSpecifier(relativeSpecifierUrl)) {
-        relativeSpecifierUrl = './' + relativeSpecifierUrl;
-      }
-      node.source.value = relativeSpecifierUrl;
     }
   }
 });
