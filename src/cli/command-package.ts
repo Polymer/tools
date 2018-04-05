96cc5c7595e29ae02ea8f2786ae92d482aa043de
Elliott Marquez <emarquez@google.com>
2018-04-04 21:12:12 -0700

    fix type

--- src/cli/command-package.ts
+++ src/cli/command-package.ts
@@ -51,7 +51,8 @@ export default async function run(options: CliOptions) {
 
   // TODO: each file is not always needed, refactor to optimize loading
   let inBowerJson:
-      {name: string, version: string, main: any, packageType: string}|undefined;
+      {name: string, version: string, main: any, packageType: PackageType}|
+      undefined;
   let inPackageJson: {name: string, version: string, packageType: PackageType}|
       undefined;
   let outPackageJson: {name: string, version: string, packageType: PackageType}|
