e13a0c68223140d6aa08aefe63b865c4df114ed8
Alexander Marks <aomarks@google.com>
2018-04-20 13:58:24 -0700

    Fix tests.

--- packages/browser-capabilities/src/test/browser-capabilities_test.ts
+++ packages/browser-capabilities/src/test/browser-capabilities_test.ts
@@ -27,13 +27,13 @@ suite('capabilities', function() {
 
   test('chrome has all the capabilities', () => {
     assertBrowserCapabilities(
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.31 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
         ['es2015', 'push', 'serviceworker', 'modules']);
   });
 
   test('chrome headless has all the capabilities', () => {
     assertBrowserCapabilities(
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/61.0.3163.31 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/66.0.3359.117 Safari/537.36',
         ['es2015', 'push', 'serviceworker', 'modules']);
   });
 
@@ -49,10 +49,10 @@ suite('capabilities', function() {
   test('safari push capability is predicated on macOS version', () => {
     assertBrowserCapabilities(
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30',
-        ['es2015', 'modules']);
+        ['es2015']);
     assertBrowserCapabilities(
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30',
-        ['es2015', 'push', 'modules']);
+        ['es2015', 'push']);
   });
 
   test('parseVersion parses with fallback to -1', () => {
