742958906a30860f5f5df5edea9f3418f603a780
Alexander Marks <aomarks@google.com>
2018-04-23 14:41:24 -0700

    Update goldens for new Babel JS.

--- packages/cli/src/test/integration/fixtures/build-with-preset/expected/es5-bundled/service-worker.js
+++ packages/cli/src/test/integration/fixtures/build-with-preset/expected/es5-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","c2865ec2cb8a0025e45bbcf9852fcc62"]];
+var precacheConfig = [["index.html","d165acd43a17a848a51a2ed64f8cd54f"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
