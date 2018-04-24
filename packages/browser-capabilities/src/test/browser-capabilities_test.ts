e45e83d81826e3cfd00d0e319a9f1a2d816973cb
Alexander Marks <aomarks@google.com>
2018-04-23 17:28:08 -0700

    Add Safari OS tests.

--- packages/browser-capabilities/src/test/browser-capabilities_test.ts
+++ packages/browser-capabilities/src/test/browser-capabilities_test.ts
@@ -46,6 +46,24 @@ suite('capabilities', function() {
         ['es2015', 'push']);
   });
 
+  test('safari push capability is predicated on macOS version', () => {
+    assertBrowserCapabilities(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30',
+        ['es2015']);
+    assertBrowserCapabilities(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30',
+        ['es2015', 'push']);
+  });
+
+  test('safari mobile modules capability is predicated on iOS version', () => {
+    assertBrowserCapabilities(
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1',
+        ['es2015', 'push']);
+    assertBrowserCapabilities(
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1',
+        ['es2015', 'push', 'modules']);
+  });
+
   test('parseVersion parses with fallback to -1', () => {
     assert.deepEqual(capabilities.parseVersion('37'), [37]);
     assert.deepEqual(capabilities.parseVersion('10.987.00.1'), [10, 987, 0, 1]);
