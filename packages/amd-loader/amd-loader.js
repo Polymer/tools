b61a884960bfa4e39165665247c8a571a0bf4807
Alexander Marks <aomarks@google.com>
2018-04-26 12:07:13 -0700

    Some refactoring.

--- packages/amd-loader/amd-loader.js
+++ packages/amd-loader/amd-loader.js
@@ -2,67 +2,37 @@
 
 (function() {
 
-var pendingInitCallback = undefined;
-var topLevelIdx = 0;
-var previousTopLevelUrl = undefined;
+function Registry() {
+  this.modules = {};
+  this.topLevelIdx = 0;
+  this.previousTopLevelUrl = undefined;
+  this.pendingDefine = undefined;
+}
 
-/**
- * Define a module and execute it when all dependencies are resolved.
- */
-window.define = window.require = function(deps, factory) {
-  // We don't yet know our own module URL. We need to discover it so that we can
-  // resolve our relative dependency specifiers. There are two ways the script
-  // executing this define() call could have been loaded:
-
-  // Case #1: We are a dependency of another module. A <script> was injected to
-  // load us, but we don't yet know the URL that was used. Scripts cannot
-  // directly access their URL, so we rely on the script that loaded us to
-  // inform us of the path it used.
-  //
-  // To do this, we register our init function to a global. Once this script has
-  // finished executing, the "onload" event will be fired on this <script>, and
-  // the script that loaded us will invoke our init function with the URL.
-  var pendingInitCalled = false;
-  pendingInitCallback = function(url) {
-    pendingInitCalled = true;
-    pendingInitCallback = undefined;
-    lookupModule(url).init(deps, factory);
-  };
-
-  // Case #2: We are a top-level script in the HTML document. Our URL is the
-  // document's base URL. We can discover this case by waiting a tick, and if we
-  // haven't already been initialized by the "onload" handler from case #1, then
-  // this must be case #2.
-  setTimeout(function() {
-    if (pendingInitCalled === false) {
-      var url = document.baseURI + '#' + topLevelIdx++;
-      var module = lookupModule(url);
-      module.loadStartedOrDone = true;
-      if (previousTopLevelUrl !== undefined) {
-        // type=module scripts execute in order (with the same timing as defer
-        // scripts). Because this is a top-level script, and we are trying to
-        // mirror type=module behavior as much as possible, inject a dependency
-        // on the previous top-level script to preserve the relative ordering.
-        deps.push(previousTopLevelUrl);
-      }
-      previousTopLevelUrl = url;
-      module.init(deps, factory);
-    }
-  }, 0);
-};
-
-var registry = {};
-
-function lookupModule(url) {
-  var module = registry[url];
+Registry.prototype.get = function(url) {
+  var module = this.modules[url];
   if (module === undefined) {
-    module = registry[url] = new Module(url);
+    module = this.modules[url] = new Module(url);
   }
   return module;
+};
+
+var registry = new Registry();
+
+function getBasePath(url) {
+  // TODO(aomarks) Maybe a better way to do this.
+  var lastSlash = url.lastIndexOf('/');
+  if (lastSlash === url.indexOf('://') + 2) {
+    // http://example.com -> http://example.com/
+    return url + '/';
+  }
+  // http://example.com/foo/bar.html -> http://example.com/foo/
+  return url.substring(0, lastSlash + 1);
 }
 
 function Module(url) {
   this.url = url;
+  this.basePath = getBasePath(url);
   this.exports = {};
   this.loadStartedOrDone = false;
   // All of this module's dependencies are resolved and its factory has run.
@@ -72,17 +42,6 @@ function Module(url) {
 }
 
 var resolverAnchor = document.createElement('a');
-
-function resolveUrl(base, path) {
-  if (path.indexOf('://') !== -1) {
-    // Already a fully qualified URL.
-    // TODO(aomarks) Is this a good enough check?
-    return path;
-  }
-  // Just let the browser do path resolution/normalization.
-  resolverAnchor.href = base + path;
-  return resolverAnchor.href;
-}
 
 Module.prototype.init = function(deps, factory) {
   var factoryArgs = [];
@@ -101,59 +60,32 @@ Module.prototype.init = function(deps, factory) {
     }
   }
 
-  var basePath = getBasePath(this.url);
-
   for (var i = 0; i < (deps || []).length; i++) {
     var depSpec = deps[i];
 
     if (depSpec === 'exports') {
       factoryArgs[i] = this.exports;
 
-    } else if (depSpec === 'url') {
-      // TODO(aomarks) This is an idea for replacing the import.meta transform.
-      factoryArgs[i] = this.url;
-
     } else if (depSpec === 'require') {
-      factoryArgs[i] = makeDynamicImporter(basePath);
+      factoryArgs[i] = this.makeDynamicImporter();
+
+    } else if (depSpec === 'meta') {
+      // TODO(aomarks) This is an idea for replacing the import.meta transform.
+      factoryArgs[i] = {url: this.url};
 
     } else {
-      var depUrl = resolveUrl(basePath, depSpec);
-      var depModule = lookupModule(depUrl);
+      var depModule = registry.get(this.resolveUrl(depSpec));
       factoryArgs[i] = depModule.exports;
 
       if (depModule.resolved === false) {
         numUnresolvedDeps++;
         depModule.notify.push(onDepResolved);
-
-        if (depModule.loadStartedOrDone === false) {
-          depModule.makeLoadScript();
-        }
+        depModule.loadOnce();
       }
     }
   }
 
   maybeResolve();
-};
-
-Module.prototype.makeLoadScript = function() {
-  this.loadStartedOrDone = true;
-  var script = document.createElement('script');
-  var url = this.url;
-  script.src = url;
-
-  /// TODO(aomarks) Error handling.
-  script.onload = function() {
-    if (pendingInitCallback !== undefined) {
-      pendingInitCallback(url);
-    } else {
-      // The script did not make a call to define(), otherwise the global
-      // callback would have been set. That's fine, we can resolve it
-      // immediately, because it doesn't have any dependencies.
-      lookupModule(url).resolve();
-    }
-  };
-
-  document.head.appendChild(script);
 };
 
 Module.prototype.resolve = function() {
@@ -163,31 +95,102 @@ Module.prototype.resolve = function() {
   }
 };
 
-function makeDynamicImporter(basePath) {
-  return function dynamicImporter(spec, callback) {
-    var url = resolveUrl(basePath, spec);
-    var module = lookupModule(url);
+Module.resolverAnchor = document.createElement('a');
+
+Module.prototype.resolveUrl = function(path) {
+  if (path.indexOf('://') !== -1) {
+    // Already a fully qualified URL.
+    // TODO(aomarks) Is this a good enough check?
+    return path;
+  }
+  // Just let the browser do path resolution/normalization.
+  Module.resolverAnchor.href = this.basePath + path;
+  return Module.resolverAnchor.href;
+};
+
+Module.prototype.makeDynamicImporter = function() {
+  var resolveUrl = this.resolveUrl.bind(this);
+  return function(spec, callback) {
+    var module = registry.get(resolveUrl(spec));
     if (module.resolved === true) {
       callback(module.exports);
     } else {
       module.notify.push(function() {
         callback(module.exports);
       });
-      if (module.loadStartedOrDone === false) {
-        module.makeLoadScript();
-      }
+      module.loadOnce();
     }
   };
-}
+};
 
-function getBasePath(url) {
-  // TODO(aomarks) Maybe a better way to do this.
-  var lastSlash = url.lastIndexOf('/');
-  if (lastSlash === url.indexOf('://') + 2) {
-    // http://example.com -> http://example.com/
-    return url + '/';
+Module.prototype.loadOnce = function() {
+  if (this.loadStartedOrDone === true) {
+    return;
   }
-  // http://example.com/foo/bar.html -> http://example.com/foo/
-  return url.substring(0, lastSlash + 1);
-}
+  this.loadStartedOrDone = true;
+
+  var script = document.createElement('script');
+  script.src = this.url;
+
+  var module = this;
+  script.onload = function() {
+    if (registry.pendingDefine !== undefined) {
+      registry.pendingDefine(module);
+    } else {
+      // The script did not make a call to define(), otherwise the global
+      // callback would have been set. That's fine, we can resolve it
+      // immediately, because it doesn't have any dependencies.
+      module.resolve();
+    }
+  };
+
+  script.onerror = function() {
+    throw new Error('error loading module', url);
+  };
+
+  document.head.appendChild(script);
+};
+
+/**
+ * Define a module and execute it when all dependencies are resolved.
+ */
+window.define = window.require = function(deps, factory) {
+  // We don't yet know our own module URL. We need to discover it so that we can
+  // resolve our relative dependency specifiers. There are two ways the script
+  // executing this define() call could have been loaded:
+
+  // Case #1: We are a dependency of another module. A <script> was injected to
+  // load us, but we don't yet know the URL that was used. Because
+  // document.currentScript is not supported by IE, we communicate the URL via a
+  // global callback. When finished executing, the "onload" event will be fired
+  // by this <script>, which will be handled by the loading script, which will
+  // invoke the callback with our module object.
+  var pendingDefineCalled = false;
+  registry.pendingDefine = function(module) {
+    pendingDefineCalled = true;
+    registry.pendingDefine = undefined;
+    module.init(deps, factory);
+  };
+
+  // Case #2: We are a top-level script in the HTML document. Our URL is the
+  // document's base URL. We can discover this case by waiting a tick, and if we
+  // haven't already been defined by the "onload" handler from case #1, then
+  // this must be case #2.
+  setTimeout(function() {
+    if (pendingDefineCalled === false) {
+      var url = document.baseURI + '#' + registry.topLevelIdx++;
+      var module = registry.get(url);
+      module.loadStartedOrDone = true;
+      if (registry.previousTopLevelUrl !== undefined) {
+        // type=module scripts execute in order (with the same timing as defer
+        // scripts). Because this is a top-level script, and we are trying to
+        // mirror type=module behavior as much as possible, inject a dependency
+        // on the previous top-level script to preserve the relative ordering.
+        deps.push(registry.previousTopLevelUrl);
+      }
+      registry.previousTopLevelUrl = url;
+      module.init(deps, factory);
+    }
+  }, 0);
+};
 })();
