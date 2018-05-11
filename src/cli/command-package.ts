ed3517e47eee26ee65972c869741ea222ea7b53e
Marcin Lulek <info@webreactor.eu>
2018-05-11 23:23:51 +0200

    Allow for running modulizer in a directory that is not git controlled (#430)

--- src/cli/command-package.ts
+++ src/cli/command-package.ts
@@ -29,8 +29,20 @@ export default async function run(options: CliOptions) {
 
   // Ok, we're updating a package in a directory not under our control.
   // We need to be sure it's safe.
-  const {stdout, stderr} = await exec(inDir, 'git', ['status', '-s']);
-  if (!options.force && (stdout || stderr)) {
+  let stdout, stderr;
+  let isRepo = true;
+  try {
+    ({stdout, stderr} = await exec(inDir, 'git', ['status', '-s']));
+  } catch (e) {
+    // Grab command execution results from exception info.
+    ({stdout, stderr} = e);
+    isRepo =
+        stderr === undefined || stderr.indexOf('Not a git repository') === -1;
+  }
+  if (!isRepo) {
+    console.warn(`Not a git repo, proceeding.`);
+  }
+  if (!options.force && isRepo && (stdout || stderr)) {
     console.error(
         `Git repo is dirty. Check all changes in to source control and ` +
         `then try again.`);
