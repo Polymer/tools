d1854fd6a98ca0bb1e6f198f9ecbb78367b757b7
Tim van der Lippe <tvanderlippe@gmail.com>
2018-07-01 00:00:25 +0200

    Fix audit warnings in wct-browser-legacy and wct

--- packages/wct-browser-legacy/browser.js
+++ packages/wct-browser-legacy/browser.js
@@ -212,9 +212,10 @@ window.waitFor = waitFor;
 var _config = {
     environmentScripts: !!window.__wctUseNpm ?
         [
-            'stacky/browser.js', 'async/lib/async.js', 'lodash/index.js',
-            'mocha/mocha.js', 'chai/chai.js', '@polymer/sinonjs/sinon.js',
+            'stacky/browser.js', 'async/dist/async.js', 'lodash/lodash.js',
+            'mocha/mocha.js', 'chai/chai.js',
             'sinon-chai/lib/sinon-chai.js',
+            'sinon/pkg/sinon.js',
             'accessibility-developer-tools/dist/js/axs_testing.js',
             '@polymer/test-fixture/test-fixture.js'
         ] :
--- packages/web-component-tester/browser/config.ts
+++ packages/web-component-tester/browser/config.ts
@@ -44,9 +44,10 @@ export interface Config {
 export let _config: Config = {
   environmentScripts: !!window.__wctUseNpm ?
       [
-        'stacky/browser.js', 'async/lib/async.js', 'lodash/index.js',
-        'mocha/mocha.js', 'chai/chai.js', '@polymer/sinonjs/sinon.js',
+        'stacky/browser.js', 'async/dist/async.js', 'lodash/lodash.js',
+        'mocha/mocha.js', 'chai/chai.js',
         'sinon-chai/lib/sinon-chai.js',
+        'sinon/pkg/sinon.js',
         'accessibility-developer-tools/dist/js/axs_testing.js',
         '@polymer/test-fixture/test-fixture.js'
       ] :
