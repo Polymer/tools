f9cf57704c91072cb8365a0e6007e445a8d518b2
Brendan Baldwin <brendan@usergenic.com>
2018-08-09 16:13:25 -0700

    cli: remove obsolete types (#627)

--- packages/cli/custom_typings/css-slam.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'css-slam' {
-
-  import {Transform} from 'stream';
-
-  export function css(text: string): string;
-  export function html(text: string): string;
-  export function gulp(): Transform;
-}
