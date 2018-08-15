57c06e1efeaa0a4a1be1806ec2613c6cc7ab5dc9
Brendan Baldwin <brendan@usergenic.com>
2018-08-15 10:12:15 -0700

    WIP commit towards getting wct-mocha setup.

---
+++ packages/wct-mocha/src/more-declarations.ts
@@ -0,0 +1,41 @@
+declare namespace Mocha {
+  namespace utils {
+    function highlightTags(somethingSomething: string): void;
+  }
+
+  interface IRunner extends NodeJS.EventEmitter {
+    name?: string;
+    total: number;
+  }
+
+  interface IRunnable {
+    parent?: ISuite;
+    root: boolean;
+    state?: 'passed'|'failed';
+    pending: boolean;
+  }
+
+  interface ISuite {
+    root: boolean;
+  }
+
+  //let Runner: {prototype: IRunner; immediately(callback: () => void): void};
+}
+
+interface Window {
+  /**
+   * A function to filter out expected uncaught errors.
+   *
+   * If this function exists, then any events fired from the window's 'error'
+   * will be passed to this function. If it returns true, then the error event
+   * will not be logged and will not cause tests to fail.
+   */
+  uncaughtErrorFilter?(errorEvent: ErrorEvent): boolean;
+}
+
+interface HTMLElement {
+  isConnected: boolean;
+}
+interface SVGElement {
+  isConnected: boolean;
+}
