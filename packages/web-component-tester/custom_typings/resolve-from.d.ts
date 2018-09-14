ce43d7ffedc4ca593f95730e964394c778d0a87e
Brendan Baldwin <brendan@usergenic.com>
2018-09-14 16:14:37 -0700

    Removed custom typings for resolve-from.d.ts since no longer using package.

--- packages/web-component-tester/custom_typings/resolve-from.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'resolve-from' {
-  function resolveFrom(fromDir: string, moduleId: string): string;
-  namespace resolveFrom {
-    function silent(fromDir: string, moduleId: string): string;
-  }
-  export = resolveFrom;
-}
