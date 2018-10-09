7d45f8c51b493989ca52913bc3ad4c36c26d8052
Brendan Baldwin <brendan@usergenic.com>
2018-10-08 23:49:50 -0700

    WIP commit.  Flaky integration tests reporter now seems to rerun only-failed-tests appropriately.

--- packages/web-component-tester/test/flaky-integration-tests-reporter.ts
+++ packages/web-component-tester/test/flaky-integration-tests-reporter.ts
@@ -1,36 +1,72 @@
-import {writeFileSync} from 'fs';
+import {spawn} from 'child_process';
 import * as Mocha from 'mocha';
-import {resolve} from 'path';
 
-function grepEscape(s: string): string {
-  return s.replace(/([^a-z0-9])/g, '\\$1');
-}
+
+const testGrepCount = parseInt(process.env['TEST_FAIL_COUNT'] + '', 10) || 0;
+const testRunCount = parseInt(process.env['TEST_RUN_COUNT'] + '', 10) || 1;
+const testRunMax = parseInt(process.env['TEST_RUN_MAX'] + '', 10) || 3;
 
 module.exports =
     class FlakyIntegrationTestsReporter extends Mocha.reporters.Spec {
   constructor(runner: Mocha.Runner) {
     super(runner);
-
-    let failedTests: {test: Mocha.Test, error: Error}[] = [];
-
-    runner.on('fail', function(test: Mocha.Test, error: Error) {
-      failedTests.push({test, error});
+    const fails = new Set<string>();
+    runner.on('fail', function(test: Mocha.Test, _error: Error) {
+      const title = test.fullTitle().replace(/^".+" hook for "(.+)"/g, '$1');
+      fails.add(title);
     });
-
     runner.on('end', function() {
-      if (failedTests.length === 0) {
-        writeFileSync(
-            resolve('./rerun-failed-integration-tests.bash'),
-            'echo "No tests to rerun.  All tests passed."');
+      if (runner.stats.failures + runner.stats.passes < testGrepCount) {
+        console.error('Failed to run intented number of tests.');
+        process.exit(1);
+      }
+      if (fails.size === 0) {
         return;
       }
-      writeFileSync(
-          resolve('./rerun-failed-integration-tests.bash'),
-          `mocha "test/integration/**/*.js" --timeout 90000 --reporter "test/flaky-integration-tests-reporter.js" --grep "${
-              failedTests.map((data) => grepEscape(data.test.fullTitle()))
-                  .join('|')}"`);  // decide if you want multiple retry case
-                                   // here || bash
-                                   // ./rerun-flaky-integration-tests.bash`);
+      if (testRunCount >= testRunMax) {
+        console.error(
+            `${testRunMax} attempts made, but still ` +
+            `have ${runner.stats.failures} failure(s).`);
+        return;
+      }
+      const args = process.argv.slice(1);
+      process.execArgv.forEach((a) => args.push(a));
+      const filteredArgs = [];
+      for (let a = 0; a < args.length; ++a) {
+        const arg = args[a];
+        if (['--grep', '-g', '--fgrep', '-f'].includes(arg)) {
+          ++a;
+          continue;
+        }
+        if (['--grep=', '-g=', 'fgrep=', '-f='].some(
+                (p) => arg.startsWith(p))) {
+          continue;
+        }
+        filteredArgs.push(arg);
+      }
+      filteredArgs.push('--grep');
+      filteredArgs.push([...fails].map(grepEscape).join('|'));
+
+      console.error(
+          `===\n` +
+          `Test run produced ${runner.stats.failures} failure(s) ` +
+          `on attempt #${testRunCount} out of ${testRunMax} limit.\n` +
+          `Rerunning failed tests:\n` +
+          [...fails].map((t) => ' - ' + t).join('\n') + `\n===\n`);
+
+      runner.abort();
+
+      spawn(process.argv0, filteredArgs, {
+        env: Object.assign({}, process.env, {
+          'TEST_GREP_COUNT': `${runner.stats.failures}`,
+          'TEST_RUN_COUNT': `${testRunCount + 1}`,
+        }),
+        stdio: 'inherit',
+      });
     });
   }
+}
+
+function grepEscape(s: string): string {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
