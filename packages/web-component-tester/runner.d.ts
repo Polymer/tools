1d932621864d7be7d5eeabffdb5794471482b01a
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-02-22 12:35:45 +1300

    Added types for web-component-tester.

---
+++ packages/web-component-tester/runner.d.ts
@@ -0,0 +1,7 @@
+import * as cli from './runner/cli';
+import * as config from './runner/config';
+import * as gulp from './runner/gulp';
+import * as steps from './runner/steps';
+import * as test from './runner/test';
+
+export {cli, config, gulp, steps, test};
