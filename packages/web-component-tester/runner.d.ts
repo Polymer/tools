8c9746536caacbf98302f229bc2d1584e3f86972
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-02-26 13:26:48 +1300

    Updated types.

--- packages/web-component-tester/runner.d.ts
+++ packages/web-component-tester/runner.d.ts
@@ -1,7 +1,42 @@
-import * as cli from './runner/cli';
-import * as config from './runner/config';
-import * as gulp from './runner/gulp';
-import * as steps from './runner/steps';
-import * as test from './runner/test';
+import * as _cli from './runner/cli';
+import * as _config from './runner/config';
+import * as _gulp from './runner/gulp';
+import * as _steps from './runner/steps';
+import * as _test from './runner/test';
 
-export {cli, config, gulp, steps, test};
+export {Browser, Config} from './runner/config'
+
+export const cli = {
+  run: _cli.run,
+  runSauceTunnel: _cli.runSauceTunnel
+};
+
+export const config = {
+  getPackageName: _config.getPackageName,
+  defaults: _config.defaults,
+  PreparsedArgs: _config.PreparsedArgs,
+  fromDisk: _config.fromDisk,
+  preparseArgs: _config.preparseArgs,
+  parseArgs: _config.parseArgs,
+  merge: _config.merge,
+  normalize: _config.normalize,
+  expand: _config.expand,
+  validate: _config.validate
+};
+
+export const gulp = {
+  init: _gulp.init
+};
+
+export const steps = {
+  setupOverrides: _steps.setupOverrides,
+  loadPlugins: _steps.loadPlugins,
+  configure: _steps.configure,
+  prepare: _steps.prepare,
+  runTests: _steps.runTests,
+  cancelTests: _steps.cancelTests
+};
+
+export const test = {
+  test: _test.test
+};
